0f465399c529a86466e36cb849e2119384c5db17
The-Last-Cookie <50201659+The-Last-Cookie@users.noreply.github.com>
2022-08-28 13:05:41 +0200

    add hysteresis value management to reactor screen

--- reactor/gui/Screens/ReactorScreen.lua
+++ reactor/gui/Screens/ReactorScreen.lua
@@ -9,12 +9,10 @@ local btnReactorOn = buttonAPI.Button()
 local btnReactorOff = buttonAPI.Button()
 local btnReactorAuto = buttonAPI.Button()
 
---[[ TODO: add hys value management to ReactorAPI
 local btnHysMinIncrease = buttonAPI.Button()
 local btnHysMinDecrease = buttonAPI.Button()
 local btnHysMaxIncrease = buttonAPI.Button()
 local btnHysMaxDecrease = buttonAPI.Button()
-]]--
 
 local function init()
     btnReactorOn.x = 4
@@ -40,6 +38,38 @@ local function init()
     btnReactorAuto.backgroundColor = 0x0000FF
     btnReactorAuto.foregroundColor = 0xFFFFFF
     btnReactorAuto.text = "Auto"
+
+    btnHysMinIncrease.x = 21
+    btnHysMinIncrease.y = 29
+    btnHysMinIncrease.width = 10
+    btnHysMinIncrease.height = 3
+    btnHysMinIncrease.backgroundColor = 0x0000FF
+    btnHysMinIncrease.foregroundColor = 0xFFFFFF
+    btnHysMinIncrease.text = "Min + 10"
+
+    btnHysMinDecrease.x = 21
+    btnHysMinDecrease.y = 33
+    btnHysMinDecrease.width = 10
+    btnHysMinDecrease.height = 3
+    btnHysMinDecrease.backgroundColor = 0x0000FF
+    btnHysMinDecrease.foregroundColor = 0xFFFFFF
+    btnHysMinDecrease.text = "Min - 10"
+
+    btnHysMaxIncrease.x = 32
+    btnHysMaxIncrease.y = 29
+    btnHysMaxIncrease.width = 10
+    btnHysMaxIncrease.height = 3
+    btnHysMaxIncrease.backgroundColor = 0x0000FF
+    btnHysMaxIncrease.foregroundColor = 0xFFFFFF
+    btnHysMaxIncrease.text = "Max + 10"
+
+    btnHysMaxDecrease.x = 32
+    btnHysMaxDecrease.y = 33
+    btnHysMaxDecrease.width = 10
+    btnHysMaxDecrease.height = 3
+    btnHysMaxDecrease.backgroundColor = 0x0000FF
+    btnHysMaxDecrease.foregroundColor = 0xFFFFFF
+    btnHysMaxDecrease.text = "Max - 10"
 end
 
 local function handleTouchEvent(eventData)
@@ -49,6 +79,14 @@ local function handleTouchEvent(eventData)
         reactorAPI.forceInactive()
     elseif buttonAPI.isClicked(btnReactorAuto, eventData) then
         reactorAPI.disableForce()
+    elseif buttonAPI.isClicked(btnHysMinIncrease, eventData) then
+        reactorAPI.setHysteresisMin(reactorAPI.getHysteresisMin() + 0.1)
+    elseif buttonAPI.isClicked(btnHysMinDecrease, eventData) then
+        reactorAPI.setHysteresisMin(reactorAPI.getHysteresisMin() - 0.1)
+    elseif buttonAPI.isClicked(btnHysMaxIncrease, eventData) then
+        reactorAPI.setHysteresisMax(reactorAPI.getHysteresisMax() + 0.1)
+    elseif buttonAPI.isClicked(btnHysMaxDecrease, eventData) then
+        reactorAPI.setHysteresisMax(reactorAPI.getHysteresisMax() - 0.1)
     end
 end
 
@@ -61,12 +99,19 @@ local function show(reactorInfo)
     drawAPI.Text(4, 19, "Fuel temperature: " .. reactorInfo.FuelTemperature .. " °C", 0x000000, 0xFFFFFF)
     drawAPI.Text(4, 21, "Waste amount: " .. reactorInfo.WasteAmount .. " mB", 0x000000, 0xFFFFFF)
 
+    local strHys = "Hysterises values : " .. reactorInfo.HysteresisMin * 100 .. " % - " .. reactorInfo.HysteresisMax * 100 .. " %"
+    drawAPI.Text(21, 27, strHys, 0x000000, 0xFFFFFF)
+
     -- TODO: show status via lamps
     drawAPI.Text(30, 7, "Reactor status: " .. reactorInfo.Status, 0x000000, 0xFFFFFF)
 
     buttonAPI.draw(btnReactorOn)
     buttonAPI.draw(btnReactorOff)
     buttonAPI.draw(btnReactorAuto)
+    buttonAPI.draw(btnHysMinIncrease)
+    buttonAPI.draw(btnHysMinDecrease)
+    buttonAPI.draw(btnHysMaxIncrease)
+    buttonAPI.draw(btnHysMaxDecrease)
 end
 
 return { Title = Title, init = init, handleTouchEvent = handleTouchEvent, show = show }
