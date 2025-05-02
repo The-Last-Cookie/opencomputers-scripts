eb05b80a829dd14d43fd669482f30a2c341c9a1c
The-Last-Cookie <50201659+The-Last-Cookie@users.noreply.github.com>
2025-05-02 21:47:18 +0200

    Add main loop

--- reactor/gui/Main.lua
+++ reactor/gui/Main.lua
@@ -3,26 +3,31 @@ local term = require("term")
 
 local logger = require "Logger"
 local program = require "Program"
-local screenAPI = require "ScreenAPI"
+local interface = require "interface"
+local reactor = require "Reactor"
+local reactorScreen = require "Screens/ReactorScreen"
 
-function init()
-    logger.log(logger.LogStatus.INFO, "Starting program")
-    print("Loading ...")
-    screenAPI.init()
-    os.sleep(program.ScreenRefreshTime)
-end
 
-init()
+logger.log(logger.LogStatus.INFO, "Starting program")
+print("Loading ...")
 
-event.listen("touch", screenAPI.handleTouchEvent)
+reactor.init()
+interface.clearAllObjects()
+interface.clearScreen()
+reactorScreen.init()
+interface.updateAll()
 
 while program.IsRunning do
-    term.clear()
-    screenAPI.display()
-    os.sleep(program.ScreenRefreshTime)
+    local name, address, x, y, button, player = event.pull(program.ScreenRefreshTime, "touch")
+
+    if x and y then
+        interface.processClick(x,y)
+    end
+
+    reactor.monitor()
+    reactorScreen.update()
 end
 
-event.ignore("touch", screenAPI.handleTouchEvent)
 term.clear()
 
 -- this would stay on false due to caching
